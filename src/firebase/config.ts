582fedb3ec4605ad63e4be7bd21dd03b7236d36f
Marcelo Giuffra <agencia@elsartenpro.com>
2025-10-13 11:19:09 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "crm-superflow",
+  "appId": "1:1000491981263:web:a07815ac22ee425d881aba",
+  "apiKey": "AIzaSyC0o1HquhKJTOVr4ui9PIBPXVYnuzYep7M",
+  "authDomain": "crm-superflow.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1000491981263"
+};
