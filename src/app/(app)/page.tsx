118094b294f7c2b928cd1a4f276f2c35e7c59efb
Marcelo Giuffra <agencia@elsartenpro.com>
2025-10-14 21:05:57 +0000

    puedes ponerlo con letras mayusculas NUEVO FLOW

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -86,7 +86,7 @@ export default function DashboardPage() {
       >
         <Button onClick={() => setIsSheetOpen(true)}>
           <Plus className="mr-2 h-4 w-4" />
-          nuevo flow
+          NUEVO FLOW
         </Button>
       </PageHeader>
       <p className="text-muted-foreground -mt-4 mb-8 text-sm md:text-base">
