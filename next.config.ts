ed30ca829bc855dbdcc18c8f235783a81ace5d87
Marcelo Giuffra <agencia@elsartenpro.com>
2025-10-16 11:06:21 +0000

    puedes agregar el logo desde la url: http://hiperflow.app.elsartenpro.co

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'http',
+        hostname: 'hiperflow.app.elsartenpro.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -21,21 +21,19 @@ export default function Icon() {
           borderRadius: '6px',
         }}
       >
-        <svg 
-            width="24" 
-            height="24" 
-            viewBox="0 0 24 24" 
+        <svg
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="M22.08 8.4H13.68V0H8.88V8.4H0.48V13.2H8.88V22.8H13.68V13.2H22.08V8.4Z" fill="url(#paint0_linear_favicon)"/>
-          <path d="M159.63 12.36L153.426 0H148.866L155.658 13.8L156.09 13.68C159.258 12.84 160.074 10.92 160.074 8.76C160.074 6.12 158.91 4.56 156.45 3.36L159.63 12.36ZM145.548 22.8L152.01 9.48L145.062 3.03999H136.95L144.15 12.6L135.39 22.8H145.548Z" fill="#1f7a3c" transform="scale(0.15) translate(-135, -2)"/>
-          <defs>
-            <linearGradient id="paint0_linear_favicon" x1="11.28" y1="0" x2="11.28" y2="22.8" gradientUnits="userSpaceOnUse">
-              <stop stopColor="#11E44F"/>
-              <stop offset="1" stopColor="#1f7a3c"/>
-            </linearGradient>
-          </defs>
+            >
+            <path
+                d="M17.5 10C17.5 14.1421 14.1421 17.5 10 17.5C5.85786 17.5 2.5 14.1421 2.5 10C2.5 5.85786 5.85786 2.5 10 2.5"
+                stroke="#11E44F"
+                strokeWidth="2"
+                strokeLinecap="round"
+            />
         </svg>
       </div>
     ),
